c9cd9de8448c8ca3663f04cb558188902f55e711
Nicolás Olivares <nicolas.olivares.g@usach.cl>
2017-05-27 23:10:23 -0400

    Implementado mapa coroplético Tarea <3.4>
    [FUNCIONANDO] Implementación de visualización de mapa cloroplético de comunas con los datos obtenidos del Backend

--- js/controllers/mapCtrl.js
+++ js/controllers/mapCtrl.js
@@ -1,36 +1,130 @@
 app.controller('MapCtrl', function($scope, getCommunesService){
+  var myaccesstoken = 'pk.eyJ1IjoicGFzdG9yZHJvZ28iLCJhIjoiY2ozMTRqNWtuMDAwNTJxbHNrdDNzaXBzaiJ9.yQPnbO88iRmQywPV-JKFXg';
   $scope.mapData = [];
   $scope.map = [];
-  $scope.fechaInicio=null;
-  $scope.fechaFinal=null;
-  initMap();
-  displayDataMap();
+  $scope.info = [];
+  $scope.legend= [];
+  $scope.recopilation =[];
+  $scope.geojson =L.geoJSON(comunas);
+  $scope.volarA = function(){
+    $scope.map.flyTo([-33.6050, -70.64]);
+  };
+
+  $scope.map = initMap();
+  addBackgroundLayer(myaccesstoken);
+  addCommunesLayer();
+  initLegend();
+  addLegendLayer();
+  initInfo();
+  addInfoLayer();
+  getMapData();
+
+  /*
+  Listeners para eventos
+  */
+  function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    $scope.info.update(layer.feature.properties);
+  }
+
+  function resetHighlight(e) {
+      $scope.geojson.resetStyle(e.target);
+      $scope.info.update();
+  }
+
+  function zoomToFeature(e) {
+    $scope.map.fitBounds(e.target.getBounds());
+  }
+
+  function getColor(d) {
+    var limite =5000;
+    var hue = 120-Math.floor(120 * d / limite);
+    return (d>limite)? "hsl(0, 100%, 50%)":(d<=limite && d>=0)? "hsl("+hue+", 100%, 50%)": "hsl(120,100%,50)";
+  }
+
+  function initLegend() {
+    $scope.legend=L.control({position: 'bottomright'});
+    $scope.legend.onAdd = function (map) {
+
+      this.div1 = L.DomUtil.create('div', 'info legend'),
+          grades = [0,250,500,750,1000,1250,1500,1750,2000,2250,2500,2750,3000,3250,3500,3750,4000,4250,4500,4750,5000],
+          labels = [];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      var i=0;
+      var largo = grades.length;
+      for ( i = 0; i < largo; i++) {
+          this.div1.innerHTML +=
+              '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+
+      return this.div1;
+    };
+  }
+
+  function addLegendLayer() {
+    $scope.legend.addTo($scope.map);
+  }
+
+  function initInfo(){
+      $scope.info = L.control({position: 'topright'});
+      $scope.info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+      };
+      // method that we will use to update the control based on feature properties passed
+      $scope.info.update = function (props) {
+        this._div.innerHTML = '<h4>Reportes de congestión</h4>' +  (props ?
+          '<b>' + props.NOM_COM + '</b><br />Provincia: '+props.NOM_PROV+'<br /> ' + $scope.mapData[String(props.COD_COMUNA)] + ' reportes'
+          : 'Pasa el mouse sobre alguna comuna');
+      };
+  }
+
+  function addInfoLayer(){
+    $scope.info.addTo($scope.map);
+  }
 
   function initMap(){
-    var myaccesstoken = 'pk.eyJ1IjoicGFzdG9yZHJvZ28iLCJhIjoiY2ozMTRqNWtuMDAwNTJxbHNrdDNzaXBzaiJ9.yQPnbO88iRmQywPV-JKFXg';
-    mapboxgl.accessToken = myaccesstoken;
-    $scope.map = L.map('mapid',{
+    return map = L.map('mapid',{
+        minZoom:9,
         zoom: 9,
         center: [-33.6050, -70.64]
     });
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={access_token}',
-    {
-      attribution: '© Mapbox | @ Futuchi',
-      access_token:myaccesstoken,
-      maxZoom: 18,
-      minZoom: 4,
-    }).addTo($scope.map);
-
-    L.geoJSON(comunas).addTo($scope.map);
   }
 
+  function addBackgroundLayer(accesstoken){
+    mapboxgl.accessToken = accesstoken;
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={access_token}',
+    {
+      attribution: '© Mapbox | @ Futuchi | '+$scope.recopilation,
+      access_token:accesstoken,
+      minZoom: 9,
+    }).addTo($scope.map);
+  }
 
+  function addCommunesLayer(){
+    $scope.geojson.addTo($scope.map);
+  }
 
   function onEachFeature(feature, layer) {
     // does this feature have a property named popupContent?
+    /*
     if (feature.properties && feature.properties.NOM_COM && feature.properties.NOM_PROV) {
         layer.bindPopup().setPopupContent('<strong>Comuna: </strong>'+ feature.properties.NOM_COM +
-        '<br><strong>Provincia: </strong>'+ feature.properties.NOM_PROV+'<hr class="hr-burritaco"><strong>Estadística: </strong>'+ $scope.mapData[String(feature.properties.COD_COMUNA)]+' reportes de congestión.');
+        '<br><strong>Provincia: </strong>'+ feature.properties.NOM_PROV);
           layer.on('mouseover',function(e){
             this.openPopup();
           });
@@ -38,33 +132,32 @@ app.controller('MapCtrl', function($scope, getCommunesService){
             this.closePopup();
           });
     }
+    */
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
   }
 
-
-  /*
-  Esta funcion modifica el color de cada feature(comuna) individualmente según el nivel de congestion
-  en cada comuna, realizando un llamado al servicio de getChoroplethData
-  */
-  function displayDataMap(){
+  function getMapData(){
     getCommunesService.getChoroplethData()
     .success(function(data){
       $scope.mapData = data;
-      $scope.fechaInicio=data["fecha inicial"];
-      $scope.fechaFinal=data["fecha termino"];
-      $scope.recopilation="Datos correspondientes al período entre: "+$scope.fechaInicio+" hasta "+$scope.fechaFinal;
+      /*
+      Rangos de colores:
+        Verde inicial: hue:120 saturation:100% lightness:50%
+        Rojo final: hue:0 saturation:100% lightness:50%
+      */
       var hue=120;
       var stringColor = null;
       var idCom = null;
       var estilo = {
         fillOpacity: 0.5
       };
-      L.geoJSON(comunas, {
+      $scope.geojson=L.geoJSON(comunas, {
         style: function(feature) {
-          /*
-          Rangos de colores:
-            Verde inicial: hue:120 saturation:100% lightness:50%
-            Rojo final: hue:0 saturation:100% lightness:50%
-          */
+
           hue =120;
           stringColor=null;
           idCom = null;
@@ -74,16 +167,10 @@ app.controller('MapCtrl', function($scope, getCommunesService){
           if (feature.properties && feature.properties.NOM_COM && feature.properties.NOM_PROV){
             idCom = String(feature.properties.COD_COMUNA);
             //Número de congestiones mayor que el rango máximo por comuna para considerarse en nivel máximo
-            if($scope.mapData[idCom]>=5000){
-              hue=0;
-              stringColor= "hsl("+hue+", 100%, 50%)";
-              estilo.fillColor= stringColor;
-            }else{
-              //Número de congetiones, entre 0 y 5000
-              hue = 120-Math.floor(120 * $scope.mapData[idCom] / 5000);
-              stringColor= "hsl("+hue+", 100%, 50%)";
-              estilo.fillColor= stringColor;
-            }
+            //Número de congetiones, entre 0 y 5000
+            //hue = 120-Math.floor(120 * $scope.mapData[idCom] / 5000);
+            stringColor= getColor($scope.mapData[idCom]);
+            estilo.fillColor= stringColor;
           }
           switch (feature.properties.COD_COMUNA) {
             case 1:   return estilo;
@@ -142,6 +229,8 @@ app.controller('MapCtrl', function($scope, getCommunesService){
         },
         onEachFeature: onEachFeature
       }).addTo($scope.map);
+      //Detalle para fecha de recopilacion de datos en el mapa
+      $scope.recopilation="Datos correspondientes al período entre: "+data["fecha inicial"]+" hasta "+data["fecha termino"];
     })
     .error(function(error){
       $scope.status = 'Error al consultar por comunas';
