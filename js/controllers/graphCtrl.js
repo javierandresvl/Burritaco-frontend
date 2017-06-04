21628ef0cc4a96e35a78c7eb922bf93b004fa8d3
Nicolas O <nico.olivaresg@hotmail.cl>
2017-06-03 23:00:57 -0400

    Implementado servicio grafo, sin estilo aplicado, solo por defecto
    Se recibe la respuesta desde backend: http://localhost:8080/Burritaco-backend/neo4j/nodes y se carga en una instancia nueva de sigma con mysigma.graph.read.
    Estilo por defecto

--- js/controllers/graphCtrl.js
+++ js/controllers/graphCtrl.js
@@ -1,67 +1,42 @@
 app.controller('GraphCtrl',function($scope,getCommunesService){
+   $scope.mysigma = new sigma();
+   getGraphData($scope.mysigma);
 
-
-    // these are just some preliminary settings
-    var g = new Graph();
-    var id = {};
-    var settings = {};
-    var renderer = {
-      container: getElementById('graph-container'),
-      type: 'canvas'
-    };
-
-   // Create new Sigma instance in graph-container div (use your div name here)
-   $scope.s = new sigma({
-     graph: g,
-     renderer: {
-      container: document.getElementById('graph-container'),
-      type: 'canvas'
-     },
-     settings: {
-       borderSize:2,
-       defaultNodeBorderColor:"red",
-       defaultHoverLabelBGColor:"#c0deed",
-       singleHover:true,
-       enableEdgeHovering: true,
-       defaultEdgeColor:"#c0deed",
-       defaultNodeColor:"#1dcaff",
-       minArrowSize:5
-     }
-   });
-
-
-
-   //testServicio();
-   getGraphData($scope.s);
 
    function getGraphData(s){
      getCommunesService.getNeo4jGraph()
      .success(function(data){
-       console.log(data);
-       // first you load a json with (important!) s parameter to refer to the sigma instance
-       sigma.parsers.json(
-            data,
-            s,
-            function(s) {
-                // this below adds x, y attributes as well as size = degree of the node
-                var i;
-                nodes = $scope.s.graph.nodes();
-                len = nodes.length;
-
-                for (i = 0; i < len; i++) {
-                    nodes[i].x = Math.random();
-                    nodes[i].y = Math.random();
-                    //nodes[i].size = $scope.s.graph.degree(nodes[i].id);
-                    //nodes[i].color = (nodes[i].type == "Commune") ? '#1dcaff' : 'orange';
-                }
-
-                // Refresh the display:
-                s.refresh();
-
-                // ForceAtlas Layout
-                //$scope.s.startForceAtlas2();
-            }
-       );
+        // these are just some preliminary settings
+        sigma.parsers.json('./js/resources/data.json', {
+          container: 'graph-container',
+          renderer: {
+              container: document.getElementById('graph-container'),
+              type: 'canvas'
+          },
+          settings: {
+              minNodeSize: 8,
+              maxNodeSize: 16
+          }
+        }, function(s) {
+          s.graph.clear();
+          s.graph.read(data);
+          // Display the nodes on a circle:
+          s.graph.nodes().forEach(function(node, i, a) {
+            node.x = Math.cos(Math.PI * 2 * i / a.length);
+            node.y = Math.sin(Math.PI * 2 * i / a.length);
+          });
+          // nodos = s.graph.nodes();
+          // var i=0;
+          // var numeroNodos = s.graph.nodes().length;
+          // for(i=0;i<numeroNodos;i++){
+          //   if(nodo[i].type == "Person"){
+          //
+          //   }else{
+          //
+          //   }
+          // }
+          s.startForceAtlas2();
+          });
      })
      .error(function(error){
        $scope.status = 'Error en la creacion de nodos';
